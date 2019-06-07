5f423353853210ff0da3a5eee32ae75f7deeba90
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-06-07 13:10:55 -0500

    clock using useRef and usEffect

--- my/src/clock.jsx
+++ my/src/clock.jsx
@@ -1,46 +1,38 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './clock.css'
 
-class Clock extends Component {
-	constructor() {
-		super ();
-		this.element = React.createRef()
-		this.other = React.createRef()
-		this.another = React.createRef()
-		this.changeStyle =  this.changeStyle.bind(this)
-	}
-	componentDidMount() {
-		window.addEventListener('load', this.changeStyle);
-	}
-	componentWillUnmount() { 
-		window.removeEventListener('load', this.changeStyle) 
-	}
-	changeStyle() {
-    // console.log(this.element)
+const Clock = () => {
+		const element = useRef(null);
+		const other = useRef(null)
+		const another = useRef(null)
+	useEffect(() => {
+		window.addEventListener('load', changeStyle())
+	});
+	
+	const changeStyle = () => {
 		const setDate = () => {
 				const now = new Date();
 				const seconds= now.getSeconds();
 				const secondsDegrees = ((seconds / 60) * 360) + 90;
 				if(secondsDegrees === 90){
-					this.element.current.style.transform = `rotate(${secondsDegrees}deg)`;
-					this.element.current.style.transition = `0s`;
+					element.current.style.transform = `rotate(${secondsDegrees}deg)`;
+					element.current.style.transition = `0s`;
 				} else {
-					// this.element.current.style.transform = `rotate(${secondsDegrees})`;
-					this.element.current.style.transition = `0s`;
-					this.element.current.style.transform = `rotate(${secondsDegrees}deg)`;
-					}
+				element.current.style.transition = `0s`;
+				element.current.style.transform = `rotate(${secondsDegrees}deg)`;
+				 	}
 				const minutes= now.getMinutes();
 				const minutesDegrees = ((minutes / 60) * 360) + 90;
-			// 	// if(minutesDegrees !== 90){
-					this.other.current.style.transform = `rotate(${minutesDegrees}deg)`;
+			  // if(minutesDegrees !== 90){
+					other.current.style.transform = `rotate(${minutesDegrees}deg)`;
 			// 		// }
 				const hours= now.getHours();
 				const hoursDegrees = ((hours / 12) * 360) + 90;
 			// 	// if(hoursDegrees !== 90){
-					this.another.current.style.transform = `rotate(${hoursDegrees}deg)`;
+					another.current.style.transform = `rotate(${hoursDegrees}deg)`;
 			// 		// }
-	// 		}
-	// 		setInterval(setDate, 1000);
+			}
+			setInterval(setDate, 1000);
   }
 // 	window.addEventListener('load', () => {
 // 	const secondHand = document.querySelector('.second-hand');
@@ -68,21 +60,19 @@ class Clock extends Component {
 // 	// if(hoursDegrees !== 90){
 // 		hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 // 	// }
-setInterval(setDate, 1000);
-}
-render() {
+// setInterval(setDate, 1000);
+// }
 	return (
 		<div className="container" >
 			<div className="clock">
 				<div className="clock-face">
-					<div ref={this.element} className="hand hour-hand"></div>
-					<div ref={this.other} className="hand min-hand"></div>
-					<div ref={this.another} className="hand second-hand"></div>
+					<div ref={ another } className="hand hour-hand"></div>
+					<div ref={ other } className="hand min-hand"></div>
+					<div ref={ element } className="hand second-hand"></div>
 				</div>
 			</div>
 		</div>
 	)
-}
 }
 
 export default Clock;
