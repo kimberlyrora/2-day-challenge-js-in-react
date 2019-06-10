545abceee65fa652bf52c79f9247e6d74f6d91f0
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-06-10 18:12:49 -0500

    Updates

---
+++ precache-manifest.5df01bbe7083624fd6cbbb2991e080c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2cc483cddf9dd0a5aa0de3de47428e",
+    "url": "/2-day-challenge-js-in-react/index.html"
+  },
+  {
+    "revision": "b2e76eb9eedbdeb103c2",
+    "url": "/2-day-challenge-js-in-react/static/css/main.63be118a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/2-day-challenge-js-in-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b2e76eb9eedbdeb103c2",
+    "url": "/2-day-challenge-js-in-react/static/js/main.0230aed2.chunk.js"
+  },
+  {
+    "revision": "a976b8b09f718435f7f0",
+    "url": "/2-day-challenge-js-in-react/static/js/runtime~main.e62677bd.js"
+  }
+]);
